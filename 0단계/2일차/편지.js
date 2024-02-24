c51e038157ee22a5c990ec4685fafa79c6b44931
jjaeho0415 <jjh0773@naver.com>
2024-02-24 21:00:04 +0900

    간결한 코드로 수정

--- 0단계/2일차/편지.js
+++ 0단계/2일차/편지.js
@@ -1,10 +1,14 @@
-function solution(message) {
-  let answer = 0;
-  for (let i = 0; i < message.length; i++) {
-    if (message[i]) {
-      answer += 1;
-    }
-  }
-  answer *= 2;
-  return answer;
-}
+// function solution(message) {
+//   let answer = 0;
+//   for (let i = 0; i < message.length; i++) {
+//     if (message[i]) {
+//       answer += 1;
+//     }
+//   }
+//   answer *= 2;
+//   return answer;
+// }
+
+const solution = (message) => message.map((x) => x.length * 2);
+
+console.log(solution(["hello my grandma!"]));
